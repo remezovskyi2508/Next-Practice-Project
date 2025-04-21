bc9623d7950166953c82a299439829ec700437bd
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2025-04-21 21:19:36 +0300

    render topic done

---
+++ src/app/components/client-component.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React from 'react';
+import ServerComponentCopy from './server-component-copy';
+
+export interface ClientComponentProps {
+  children?: React.ReactNode;
+}
+
+export default function ClientComponent({ children }: ClientComponentProps) {
+  return (
+    <div>
+      <span>Client Component</span>
+      {children}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import AddCompanyButton from './components/add-company-button';
-import StatusLabel, { Status } from './components/status-label';
+import ClientComponent from './components/client-component';
+import ServerComponent from './components/server-component';
+import ServerComponentCopy from './components/server-component-copy';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl mb-[30px]">Home Page</h1>
       <AddCompanyButton />
+      <ServerComponent />
+      <ClientComponent>
+        <ServerComponentCopy />
+      </ClientComponent>
     </main>
   );
 }
