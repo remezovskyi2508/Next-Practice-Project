45d56ab7bd38da2cc9e3420dd547ae72dbd89deb
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2025-04-21 20:58:26 +0300

    fix global css + render topic almost done

---
+++ src/app/components/add-company-button.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import React, { useState } from 'react';
+import Button from './button';
+import dynamic from 'next/dynamic';
+const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
+  ssr: false,
+});
+
+export default function AddCompanyButton() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Add company</Button>
+      <CompanyFormModal
+        onSubmit={console.log}
+        show={show}
+        onClose={() => setShow(false)}
+      ></CompanyFormModal>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import StatusLabel, {Status} from './components/status-label';
+import AddCompanyButton from './components/add-company-button';
+import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl mb-[30px]">Home Page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <AddCompanyButton />
     </main>
   );
 }
