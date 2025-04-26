a1c8c1318a3d546237c85af9e3602123790aa8cd
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2025-04-26 23:57:28 +0300

    finish navigation

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from '@/app/components/sidebar';
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  console.log('rendering', new Date());
+  
+  return (
+    <>
+      <Sidebar />
+      <div className="ml-60">{children}</div>
+    </>
+  );
+}
